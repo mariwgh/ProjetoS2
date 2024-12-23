05e72d5113acbaeea96e8e9ad75f7f088ca485e7
mari <133607739+mariwgh@users.noreply.github.com>
2024-12-23 01:48:25 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const div = document.getElementById('inicial');
+
 let startX = 0; // Ponto inicial do toque
-let texts = ['Projeto S2', 'Outro Texto', 'Mais um Texto']; // Textos que serão exibidos
+
+let texts = ['Eu', 'te', 'amo']; 
+
 let currentIndex = 0;
 
 div.addEventListener('touchstart', (e) => {
@@ -11,11 +14,12 @@ div.addEventListener('touchend', (e) => {
   const endX = e.changedTouches[0].clientX; // Registra o ponto final do toque
   const deltaX = endX - startX; // Calcula a diferença do movimento
 
+  // Deslizou para a direita
   if (deltaX > 50) {
-    // Deslizou para a direita
     currentIndex = (currentIndex - 1 + texts.length) % texts.length; // Vai para o texto anterior
-  } else if (deltaX < -50) {
-    // Deslizou para a esquerda
+  } 
+  // Deslizou para a esquerda
+  else if (deltaX < -50) {
     currentIndex = (currentIndex + 1) % texts.length; // Vai para o próximo texto
   }
 
